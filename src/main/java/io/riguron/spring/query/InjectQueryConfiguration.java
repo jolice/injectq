57d5431b42d8cd7eefc8ad778f472a621bfd2574
Vladimir Alexeev <25826296+riguron@users.noreply.github.com>
2020-02-14 13:52:41 +0300

    Test fixes

--- src/main/java/io/riguron/spring/query/InjectQueryConfiguration.java
+++ src/main/java/io/riguron/spring/query/InjectQueryConfiguration.java
@@ -3,7 +3,7 @@ package io.riguron.spring.query;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@ComponentScan("io.riguron.spring.query")
+@ComponentScan
 @Configuration
 public class InjectQueryConfiguration {
 }
--- src/test/java/io/riguron/spring/query/TestConfiguration.java
+++ src/test/java/io/riguron/spring/query/TestConfiguration.java
@@ -1,2 +1,9 @@
+package io.riguron.spring.query;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootConfiguration
+@ComponentScan
 public class TestConfiguration {
 }
